8793219eff592dfa1c0a42048d334d6ed76a3676
A3Tom <Tom_Esq@Yahoo.com>
2021-04-28 19:52:22 +0100

    Added some aliases I wasn't previously source controlling

---
+++ geezthem.bat
@@ -0,0 +1,3 @@
+@echo off
+
+powershell.exe -ExecutionPolicy Unrestricted -Command "TimeCmd.ps1 -Target \"C:\Program Files (x86)\Cosmic Safari\GeezYerScreensavers\GeezYerScreensavers.exe\""
---
+++ python.bat
@@ -0,0 +1,3 @@
+@echo off
+echo.
+py %*
