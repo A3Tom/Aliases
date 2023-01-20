8e933a1fb91378accac5a39f8586f33e9c833261
Gaetan Fache <gfache.work@gmail.com>
2023-01-20 16:58:01 +0000

    Display free space of the drive before and after cleaning in consecration-dotnet script

--- consecration-dotnet.bat
+++ consecration-dotnet.bat
@@ -1,3 +1,12 @@
 @echo off
 
+SET out=
+dir %CD:~0,3%|find /i "bytes free"
+echo %out%
+
+SET out=
 for /d /r . %%d in (bin,obj) do @if exist "%%d" rd /s/q "%%d\"
+
+SET out=
+dir %CD:~0,3%|find /i "bytes free"
+echo %out%
